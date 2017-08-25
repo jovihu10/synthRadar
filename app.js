886a11a8a97ab913cee55c98b632aa57edd384b8
Joël Vila <joelvila@MacBook-Pro-de-Joel.local>
2017-08-25 11:52:51 +0200

    heroku

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const path=require("path")
 const app = express()
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3002
 
-app.use(express.static(path.join(__dirname, '..client')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(PORT)
