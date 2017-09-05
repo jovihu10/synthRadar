b4174f55c5e139433f1fd440a8491e4455f72304
Joël Vila <joelvila@MacBook-Pro-de-Joel.local>
2017-09-05 16:33:22 +0200

    Buscador funciona.Trabajando item.pug

--- server/routes/root/handlers/search.js
+++ server/routes/root/handlers/search.js
@@ -4,14 +4,17 @@ function search(req, res) {
     const title = req.query.title
 
     Synth
+    	//Regex para que lo coja aunque los campos no
         .find({ title: new RegExp(title, 'i') })
+        //Para ordenarlo en orden descendiente
         .sort({ title: 1 })
         .exec(function(err, synths) {
             if (err) return res.send('error on search')
 
             if (synths.length === 0) return res.send('no results')
 
-            res.send(synths)
+            res.render('pages', {synths})
+
         })
 }
 
